4dfc30b31a5a60160edc31945463cf02bf49f7aa
Casper Thomsen <casperpvp@gmail.com>
2021-12-03 21:41:16 +0100

    Updated start.sh for Laravel

--- nginx/laravel/start.sh
+++ nginx/laravel/start.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
 echo "Navigating to Laravel..."
-cd /home/container/laravel/
+cd /home/container/public_www/
 
 echo "Updating with composer..."
 composer update
 
+echo "Clearing config..."
+php artisan config:clear
+
+if [ ! -f .env ]; then
+    echo "Copying .env exmaple file"
+    cp .env.example .env
+    echo "Generating key"
+    php artisan key:generate
+fi
+
 echo "Starting Laravel..."
-php artisan serve
+php artisan serve --host 0.0.0.0 --port ${SERVER_PORT}
